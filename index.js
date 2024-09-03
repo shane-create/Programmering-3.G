cf75ae61f68ad77363fe474acf9d41d86bb56d90
Shaila Kafi Khan <63456873+shane-create@users.noreply.github.com>
2024-09-03 08:46:22 +0200

    added web app. Will finally make it now. hehe

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var eksamensB = document.getElementById("eksamens-b");
 var class1B = document.getElementById("class1-b");
+var webApp = document.getElementById("web-app");
 
 eksamensB.onclick = function () {
   window.location.href =
@@ -9,4 +10,9 @@ eksamensB.onclick = function () {
 class1B.onclick = function () {
   window.location.href =
     "https://shane-create.github.io/Programmering-3.G/class1/class1.html";
+};
+
+webApp.onclick = function () {
+  window.location.href =
+    "https://shane-create.github.io/Programmering-3.G/web-app/web-app.html";
 };
